3bd34431fa94c253a0d266a7668677cc6942a12d
Tiago Lisboa <tiago.caio.ol@gmail.com>
2020-03-03 11:52:18 -0300

    auth layout
    created auth layout style and signin page

--- src/pages/SingIn/index.js
+++ src/pages/SingIn/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
+import logo from '../../assets/img/gobarber_logo.svg';
+import { Link } from 'react-router-dom';
+
 // import { Container } from './style';
 
 export default function SingIn() {
-  return <h1>LogIn</h1>;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </form>
+    </>
+  );
 }

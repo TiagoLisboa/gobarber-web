63c1968f2ce665e892a7da704857bd46c2a07bd1
Tiago Lisboa <tiago.caio.ol@gmail.com>
2020-03-03 11:57:06 -0300

    SingUp Page

--- src/pages/SingUp/index.js
+++ src/pages/SingUp/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
+import logo from '../../assets/img/gobarber_logo.svg';
+import { Link } from 'react-router-dom';
+
 // import { Container } from './style';
 
 export default function SingUp() {
-  return <div />;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input placeholder="Seu nome completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
